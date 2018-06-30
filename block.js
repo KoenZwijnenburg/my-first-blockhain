39fc5ea1567004a9095c190100e46f1316a3206a
Koen Zwijnenburg <koen.zwijnenburg@nepgroup.com>
2018-06-30 11:36:31 +0200

    setup + first block

---
+++ block.js
@@ -0,0 +1,21 @@
+class Block {
+
+
+    constructor(timestamp, lastHash, hash, data) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+    toString() {
+        return `Block -
+        Timestamp: ${this.timestamp}
+        last hash: ${this.lastHash.substr(0, 10)}
+        Hash: $ {this.hash.substr(0, 10)}
+        Data: ${this.data}
+        `;
+    }
+}
+
+module.exports = Block;
