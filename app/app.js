504a1ddd31515334a8398c555e4de989954ddad5
Koen Zwijnenburg <koen.zwijnenburg@nepgroup.com>
2018-07-18 15:54:03 +0200

    create multiple local servers to test

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain/blockchain');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
 const app = express();
-
-
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
 
@@ -19,11 +20,12 @@ app.post('/mine', (req, res) => {
    const block = bc.addBlock(req.body.data);
    console.log('New block was addes' + block.toString());
 
+   p2pServer.syncChains();
    res.redirect('/blocks');
 });
-
-
 
 app.listen(HTTP_PORT, () => {
     console.log('listingen on port ' + HTTP_PORT)
 });
+
+p2pServer.listen();
